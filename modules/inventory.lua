e6314e0fcb757c4d1e61c8270b396450944b629e
Julian <julzso23@gmail.com>
2015-08-26 22:53:07 +0100

    Added Inventory class

---
+++ modules/inventory.lua
@@ -0,0 +1,29 @@
+local class = require('lib.middleclass')
+
+local Inventory = class('Inventory')
+
+function Inventory:initialize(size)
+    self.size = size
+    self.items = {}
+end
+
+function Inventory:addItem(item)
+    if #self.items < self.size then
+        table.insert(self.items, item)
+    end
+end
+
+function Inventory:removeItem(id)
+    table.remove(self.items, id)
+end
+
+function Inventory:consumeItem(itemClass)
+    for k, v in pairs(self.items) do
+        if v.name == itemClass.name then
+            table.remove(self.items, k)
+            return
+        end
+    end
+end
+
+return Inventory
