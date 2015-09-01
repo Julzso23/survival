826dc217fa15af5ef77ff2c798329225b2720427
Julian <julzso23@gmail.com>
2015-09-01 01:07:34 +0100

    Tile class now extends Transformable

--- modules/Tile.lua
+++ modules/Tile.lua
@@ -2,10 +2,11 @@ local class = require('lib.middleclass')
 
 local Transformable = require('modules.Transformable')
 
-local Tile = class('Tile')
+local Tile = class('Tile', Transformable)
 
-function Tile:initialize()
+function Tile:initialize(gridSize)
     Transformable.initialize(self)
+    self.size = gridSize
 end
 
 function Tile:update(dt)
