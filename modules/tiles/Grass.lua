fcedfd35ea6fef15bd6fa723cb9aa5cbfdbdcac3
Julian <julzso23@gmail.com>
2015-09-01 01:08:12 +0100

    Draw green square as placeholder for Grass tile

--- modules/tiles/Grass.lua
+++ modules/tiles/Grass.lua
@@ -4,14 +4,16 @@ local Tile = require('modules.Tile')
 
 local Grass = class('Grass', Tile)
 
-function Grass:initialize()
-    Tile.initialize(self)
+function Grass:initialize(gridSize)
+    Tile.initialize(self, gridSize)
 end
 
 function Grass:update(dt)
 end
 
 function Grass:draw()
+    love.graphics.setColor(0, 200, 0, 255)
+    love.graphics.rectangle('fill', self:getPosition().x, self:getPosition().y, self.size, self.size)
 end
 
 return Grass
