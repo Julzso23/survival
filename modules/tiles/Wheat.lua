76843b045fb5dd4c50f04a4bacbcb38d4903ce3b
Julian <julzso23@gmail.com>
2015-09-01 18:39:56 +0100

    Added Wheat tile class

---
+++ modules/tiles/Wheat.lua
@@ -0,0 +1,32 @@
+local class = require('lib.middleclass')
+
+local Tile = require('modules.Tile')
+
+local Wheat = class('Wheat', Tile)
+
+Wheat.growTime = 10
+
+function Wheat:initialize(gridSize, x, y)
+    Tile.initialize(self, gridSize, x, y)
+    self.timer = 0
+    self.grown = false
+end
+
+function Wheat:update(dt)
+    if self.timer >= self.growTime then
+        self.grown = true
+        self.timer = self.growTime
+    else
+        self.timer = self.timer + dt
+    end
+end
+
+function Wheat:draw()
+    love.graphics.setColor(240, 255, 150, 255)
+    love.graphics.rectangle('fill', self:getPosition().x, self:getPosition().y, self.size, self.size)
+
+    love.graphics.setColor(255, 0, 0, 255)
+    love.graphics.rectangle('fill', self:getPosition().x, self:getPosition().y + self.size / 2 - 5, self.size * (self.timer / self.growTime), 10)
+end
+
+return Wheat
