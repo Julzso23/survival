d7d9c6c2e2f4c44e9bcf2dd1b8da2a8c58b678a7
Julian <julzso23@gmail.com>
2015-09-01 18:54:33 +0100

    Add player object into World class

--- modules/World.lua
+++ modules/World.lua
@@ -1,13 +1,18 @@
 local class = require('lib.middleclass')
 
+local Player = require('modules.Player')
+
 local World = class('World')
 
 function World:initialize(gridSize)
     self.gridSize = gridSize or 64
     self.tiles = {}
+    self.player = Player:new(32, 32)
 end
 
 function World:update(dt)
+    self.player:update(dt)
+
     for k, v in pairs(self.tiles) do
         v:update(dt)
     end
@@ -17,6 +22,8 @@ function World:draw()
     for k, v in pairs(self.tiles) do
         v:draw()
     end
+
+    self.player:draw()
 end
 
 function World:hasTile(x, y)
@@ -29,14 +36,19 @@ function World:hasTile(x, y)
 end
 
 function World:addTile(tileClass, x, y)
-    self:removeTile(x, y)
-
-    table.insert(self.tiles, tileClass:new(self.gridSize, x, y))
+    local exists, id = self:hasTile(x, y)
+    if not exists then
+        table.insert(self.tiles, tileClass:new(self.gridSize, x, y))
+    end
 end
 
 function World:removeTile(x, y)
     local exists, id = self:hasTile(x, y)
     if exists then
+        local drop = self.tiles:getDrop()
+        if drop then
+            self.player.inventory:addItem(drop)
+        end
         table.remove(self.tiles, id)
     end
 end
